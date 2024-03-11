c43fef203bcf0e584b39928be80d3620d057328e
Ave <159603384+AveTavern@users.noreply.github.com>
2024-03-11 22:24:13 +0800

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -4,6 +4,17 @@ import subprocess
 ispmanager_user = input("Print user that is used to execute script\n")
 os.remove("/"+ispmanager_user+"/launcher.py")
 
+if os.path.exists("/var/www/yandex-integration/deleteme.txt"):
+        file = open('/var/www/yandex-integration/deleteme.txt', 'r')
+        file = read()
+        if file == "yandex-addon":
+                file = close()
+                os.remove("/var/www/yandex-integration/deleteme.txt")
+        else:
+                print(file+" idk what is it")
+else:
+        print("no adons installed")
+
 reboot_validation = input("You should reboot server. Can we reboot now? Y / N\n")
 if reboot_validation == "Y":
         print("Reboot confirmed, please wait")
